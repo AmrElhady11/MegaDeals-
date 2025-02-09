7202059b0a3a275595f4b87eedad73328b7b72dd
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-02-09 15:02:17 +0200

    renaming megaDealsApp main class

--- src/main/java/com/MegaDeals/MegaDealsApp.java
+++ src/main/java/com/MegaDeals/MegaDealsApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AmazonEl8alabaApplication {
+public class MegaDealsApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AmazonEl8alabaApplication.class, args);
+		SpringApplication.run(MegaDealsApp.class, args);
 	}
 
 }
