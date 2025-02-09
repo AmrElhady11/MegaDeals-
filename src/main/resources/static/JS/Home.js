7bf128daaa014a0a1bff22ff171f5920640dc166
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-02-09 16:23:28 +0200

    implement add to cart button functionality using Js

---
+++ src/main/resources/static/JS/Home.js
@@ -0,0 +1,10 @@
+function handleCartAction(productId) {
+    const button = document.getElementById('cartButton-' + productId);
+
+    if (!button.dataset.cartAdded) {
+        button.dataset.cartAdded = true; // Mark this button as added to cart
+        button.textContent = 'Go to Cart'; // Change the button text
+    } else {
+        window.location.href = '/cart'; // Redirect to cart
+    }
+}
