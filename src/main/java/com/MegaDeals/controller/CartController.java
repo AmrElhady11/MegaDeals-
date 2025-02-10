32f54cc226871e77e6ffb6eab25cfa2559a0c7df
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-02-10 20:04:18 +0200

    creating cart HTML page

---
+++ src/main/java/com/MegaDeals/controller/CartController.java
@@ -0,0 +1,25 @@
+package com.MegaDeals.controller;
+
+import com.MegaDeals.model.CartItemDto;
+import com.MegaDeals.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class CartController {
+    private final CartService cartService;
+    @Autowired
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+    @GetMapping("/cart")
+    public String cart(Model model) {
+        List<CartItemDto> cartItemsList = cartService.getCartItems(1);
+        model.addAttribute("cartItemsList",cartItemsList);
+        return "CartManagement/Cart";
+    }
+}
