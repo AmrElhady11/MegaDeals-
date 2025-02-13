a54ca9ceda050ba6fdaec8fdb406f5c5cf37ff53
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-02-13 13:53:57 +0200

    create method findByCustomerId in OrderRepository through Query annotation

--- src/main/java/com/MegaDeals/repository/OrderRepository.java
+++ src/main/java/com/MegaDeals/repository/OrderRepository.java
@@ -2,8 +2,14 @@ package com.MegaDeals.repository;
 
 import com.MegaDeals.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+    @Query(value = "from Order where Order.customerID.id=:customerID")
+    List<Order> findByCustomerId(@Param("customerID") int customerId);
 }
--- src/main/java/com/MegaDeals/service/impl/OrderServiceImpl.java
+++ src/main/java/com/MegaDeals/service/impl/OrderServiceImpl.java
@@ -1,11 +1,27 @@
 package com.MegaDeals.service.impl;
 
 import com.MegaDeals.model.OrderDto;
+import com.MegaDeals.repository.OrderRepository;
+import com.MegaDeals.repository.ProductRepository;
 import com.MegaDeals.service.OrderService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class OrderServiceImpl implements OrderService {
+    private final OrderRepository orderRepository;
+    private final ProductRepository productRepository;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public OrderServiceImpl(OrderRepository orderRepository, ProductRepository productRepository, ModelMapper modelMapper) {
+        this.orderRepository = orderRepository;
+        this.productRepository = productRepository;
+        this.modelMapper = modelMapper;
+    }
+
     @Override
     public List<OrderDto> getOrders() {
         return List.of();
